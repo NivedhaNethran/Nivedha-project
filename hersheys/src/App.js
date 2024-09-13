82bd529d88a211bd96a55f295cb9c4b626abb051
NivedhaNethran <nivedhacse08@gmail.com>
2024-09-13 14:54:07 +0530

    13/9/24

--- hersheys/src/App.js
+++ hersheys/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes, useParams} from 'react-router-dom'
 import Home from './pages/Home';
 import Cakes from './pages/Cakes';
 import Chocalate from './pages/Chocalate';
 import Gifts from './pages/Gifts';
 import Cart from './pages/Cart';
 import Sweet from './pages/Sweet';
+import Admin from './admin/Admin';
+import Upload from './admin/Upload';
+import Edit from './admin/Edit';
+
+
 
 
 
@@ -25,6 +30,10 @@ function App() {
       <Route path='/gifts' element={<Gifts/>}/>
       <Route path='/sweet' element={<Sweet/>}/> 
       <Route path='/cart' element={<Cart/>}/>
+      <Route path='/admin' element={<Admin/>}/>
+      <Route path='/upload' element={<Upload/>}/>
+      <Route path='/edit/:id' element={<Edit/>} loader={({params})=>fetch(`http://localhost:4050/product/${params.id}`)}/>
+      
      
 
      </Routes>
